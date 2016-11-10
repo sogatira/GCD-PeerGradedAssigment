fb5c35036025b5dfa6ed67c265b592d07b444bb8
sogatira <sogatira@gmail.com>
2016-11-10 15:23:02 +0100

    actualizacion 1

--- run_analysis.R
+++ run_analysis.R
@@ -37,6 +37,32 @@ data <- data.frame(activity, data)
 
 rm(activity)
 
+# We select only the variables that contain mean() or std() in their names
+
+library(dplyr)
+
+data <- tbl_df(data)
+data <- select(data, grep("mean()|std()", names(data)))
+
+# We remove dots on names of variables
+
+names(data) <- gsub(pattern = "\\.", replacement = "", x = names(data))
+
+# we change t in variables for time, and f for fft to do more explicative their names
+
+names(data) <- gsub(pattern = "^t", replacement = "time", x = names(data))
+names(data) <- gsub(pattern = "^f", replacement = "fft", x = names(data))
+
+# Maybe we should change the Acc and Gyro characters in the names of variables to Accelerometer and Gyroscope, but
+# I consider that this do the names too large, but if you want to do this, you only have to uncomment this code
+
+# names(data) <- gsub(pattern = "Acc", replacement = "accelerometer", x = names(data))
+# names(data) <- gsub(pattern = "Gyro", replacement = "gyroscope", x = names(data))
+
+# Finally we 
+
+
+
 
 
 
